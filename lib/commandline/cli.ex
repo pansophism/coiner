55be05519d9e0e1d4058bd01e197d6400ca82c50
pansophism <pansophism@gmail.com>
2017-12-13 22:54:21 -0800

    fix time padding

--- lib/commandline/cli.ex
+++ lib/commandline/cli.ex
@@ -1,8 +1,25 @@
 
 defmodule Commandline.CLI do
+
+  def zero_pad(number, amount \\ 2) do
+     number
+       |> Integer.to_string
+       |> String.pad_leading(amount, ["0"])
+  end
+
+  defp now_to_string() do
+      {{year, month, day}, {hour, minute, second}} = :calendar.local_time()
+      "update time : #{year}.#{month |> zero_pad}.#{day} #{hour |> zero_pad}:#{minute |> zero_pad}:#{second |> zero_pad}"
+  end
+
   def main(args) do
-    {opts, _, _} = OptionParser.parse(args, switches: [file: :string], aliases: [f: :file])
-    IO.inspect opts
+    {opts, _, _} = OptionParser.parse(args, switches: [num: :integer], aliases: [n: :num])
+
+    opts = Keyword.merge([num: 10], opts) |> Enum.into(%{})
+
+    n = opts[:num]
+
+    IO.puts now_to_string()
 
     response = HTTPotion.get("https://api.coinmarketcap.com/v1/ticker/")
 
@@ -20,7 +37,7 @@ defmodule Commandline.CLI do
                     field == "24h_volume_usd" ->
                         {intVal, _} = Integer.parse(v)
                         r = intVal / 1000000
-                        Float.to_string(r) <> " M"
+                        Float.to_string(r) <> "M"
                     field =~ "percent" ->
                         v <> "%"
                     true ->
@@ -29,7 +46,7 @@ defmodule Commandline.CLI do
             end)
         end)
 
-    TableRex.Table.new(Enum.take(rows, 10), header)
+    TableRex.Table.new(Enum.take(rows, n), header)
         |> TableRex.Table.put_column_meta(0, color: :yellow)
         |> TableRex.Table.put_column_meta(1, color: :green)
         |> TableRex.Table.put_column_meta(2, color: :yellow)
